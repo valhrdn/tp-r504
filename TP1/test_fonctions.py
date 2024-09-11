72969c19ae13e4a847cb0346fff381906d72f7aa
Hardouin <valentinhardouin27@gmail.com>
2024-09-11 09:35:46 +0200

    Ajout TP

---
+++ TP1/test_fonctions.py
@@ -0,0 +1,6 @@
+from fonctions import puissance
+
+# Test de la fonction puissance
+print(puissance(2, 3))  # Devrait afficher 8
+print(puissance(5, 2))  # Devrait afficher 25
+print(puissance(3, 0))  # Devrait afficher 1
